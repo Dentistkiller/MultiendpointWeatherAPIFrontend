20df1f55c6a6e4b76602ec83bb2077c2d96be655
Yusuf <yusufparuk895@gmail.com>
2025-05-23 13:26:17 +0200

    Added other endpoints

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -15,6 +15,7 @@
         }
 
         public IActionResult Index() => View();
+        [HttpGet]
 
         [HttpPost]
         public async Task<IActionResult> GetWeather(string city)
@@ -30,6 +31,31 @@
             ViewBag.Reversed = reversed;
             return View("ReverseResult");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(int a, int b)
+        {
+            var result = await _apiService.AddNumbersAsync(a, b);
+            ViewBag.SumResult = result;
+            return View("AddResult");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Time()
+        {
+            var currentTime = await _apiService.GetCurrentTimeAsync();
+            ViewBag.CurrentTime = currentTime;
+            return View("TimeResult");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Random()
+        {
+            var random = await _apiService.GetRandomNumberAsync();
+            ViewBag.RandomNumber = random;
+            return View("RandomResult");
+        }
+
     }
 
 }
--- Services/ApiService.cs
+++ Services/ApiService.cs
@@ -26,7 +26,6 @@
             }
             return null;
         }
-
         public async Task<string> ReverseTextAsync(string text)
         {
             var body = JsonSerializer.Serialize(new { text });
@@ -37,6 +36,42 @@
 
             using var doc = JsonDocument.Parse(json);
             return doc.RootElement.GetProperty("reversed").GetString();
+        }
+
+        public async Task<int?> AddNumbersAsync(int a, int b)
+        {
+            var response = await _http.GetAsync($"/add?a={a}&b={b}");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                using var doc = JsonDocument.Parse(json);
+                return doc.RootElement.GetProperty("result").GetInt32();
+            }
+            return null;
+        }
+
+        public async Task<string> GetCurrentTimeAsync()
+        {
+            var response = await _http.GetAsync("/time");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                using var doc = JsonDocument.Parse(json);
+                return doc.RootElement.GetProperty("time").GetString();
+            }
+            return null;
+        }
+
+        public async Task<int?> GetRandomNumberAsync()
+        {
+            var response = await _http.GetAsync("/random");
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                using var doc = JsonDocument.Parse(json);
+                return doc.RootElement.GetProperty("random").GetInt32();
+            }
+            return null;
         }
     }
 }
